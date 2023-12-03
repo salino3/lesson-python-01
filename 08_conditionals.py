1a55a10337ac4068d585ece6ccb3f6fe75494bf9
salino3 <programacionflavio@gmail.com>
2023-12-03 15:23:37 +0100

    conditionals

---
+++ 08_conditionals.py
@@ -0,0 +1,56 @@
+
+
+my_condition = True
+
+
+
+if my_condition:
+    print("It executes the if condition")
+
+if my_condition == False:
+    print("It executes the if condition if it is false")
+
+my_condition = 5 * 3
+
+if my_condition == 10:
+    print("Igual than 10")
+if my_condition >= 10:
+    print("Major or igual than 10")
+elif my_condition > 10 and my_condition < 20:
+    print("Major than 10")
+
+if my_condition:
+    print("Hi")
+else:
+    print("Hola")
+print('Here')
+
+my_condition = True
+
+if type(my_condition) == bool:
+    print("Type is bool")
+
+my_string = "Hello"
+string_empty = ""
+
+if not my_string:
+    print("This string has a value")
+if my_string == "Hello":
+    print("Match!")
+
+if not string_empty:
+    print("¡¡Oh!!")
+
+print("The execution continuos")
+
+    
+
+
+
+
+
+
+
+
+
+
